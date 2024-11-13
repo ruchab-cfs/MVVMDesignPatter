5a5be452190b8f7c8cb8d4598a85a7aec5944daf
Rucha Bhopale <ruchab@coreflexsolutions.com>
2024-11-13 18:25:02 +0530

    Initial Commit

---
+++ MVVMDesignPatter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDesignPatter
+//
+//  Created by apple on 13/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
